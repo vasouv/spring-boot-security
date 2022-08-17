bac0af265154547bc08597e8fecb2f3ac8038f46
Vasilis Souvatzis <vasouv@gmail.com>
2022-08-17 23:25:12 +0300

    basic auth default configuration

--- src/main/java/vs/springbootsecurity/SpringBootSecurityApplication.java
+++ src/main/java/vs/springbootsecurity/SpringBootSecurityApplication.java
@@ -2,6 +2,8 @@ package vs.springbootsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringBootSecurityApplication {
@@ -10,4 +12,33 @@ public class SpringBootSecurityApplication {
 		SpringApplication.run(SpringBootSecurityApplication.class, args);
 	}
 
+}
+
+@RestController
+class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "Home Controller";
+	}
+
+}
+
+@RestController
+class UnsecuredController {
+
+	@GetMapping("unsecured")
+	public String unsecured() {
+		return "Unsecured Controller";
+	}
+
+}
+
+@RestController
+class SecuredController {
+
+	@GetMapping("secured")
+	public String secured() {
+		return "Secured Controller";
+	}
 }
